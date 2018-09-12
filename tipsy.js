c80ce83a7cb354be8336edfd3e5d4251a66ead54
Brandon <bculver3@gatech.edu>
2018-09-12 12:22:47 -0400

    choropleth initial setup

---
+++ tipsy.js
@@ -0,0 +1,26 @@
+var width = 960;
+var height = 600;
+
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var path = d3.geoPath();
+var projection = d3.geoNaturalEarth()
+    .scale(width / 2 / Math.PI)
+    .translate([width / 2, height / 2])
+var path = d3.geoPath()
+    .projection(projection);
+
+// Data and color scale
+var data = d3.map();
+var colorScheme = d3.schemeReds[6];
+colorScheme.unshift("#eee")
+var colorScale = d3.scaleThreshold()
+    .domain([1, 6, 11, 26, 101, 1001])
+    .range(colorScheme);
+
+d3.queue()
+    .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
+    .defer(d3.csv, "data.csv", function(d) { data.set(d., d.Liters); })
+    .await(ready);
